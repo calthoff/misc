356e448bbde3e27d0be14471a53275da7a120ca3
cory <coryedwardalthoff@gmail.com>
2015-02-06 14:10:02 -0800

    added stack

--- chat_message.py
+++ chat_message.py
@@ -4,6 +4,20 @@ import urllib2
 import json
 
 
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+
 class ChatMessage():
     def __init__(self, string):
         self.string = string
@@ -17,7 +31,10 @@ class ChatMessage():
         http_index_list = [m.start() for m in re.finditer('http://', self.string)]
         https_index_list = [m.start() for m in re.finditer('https://', self.string)]
         mention_flag = False
-        emoticon_flag = False
+        emoticon_stack = Stack()
+        temp_emoji_list = []
+        temp_mention_list = []
+        temp_link_list = []
         link_flag = False
         link = ""
         position = 0
@@ -27,34 +44,33 @@ class ChatMessage():
             # mentions
             mention_sc = re.match('[\w]', c)
             if mention_flag and mention_sc:
-                temp_list.append(c)
+                temp_mention_list.append(c)
             if mention_flag and not mention_sc or mention_flag and last_position:
-                self.mentions_master_list.append(''.join(temp_list))
+                self.mentions_master_list.append(''.join(temp_mention_list))
                 mention_flag = False
-                temp_list = []
+                temp_mention_list = []
             if c == '@':
                 mention_flag = True
             # emoticons
-            if c == ')':
-                if len(temp_list) <= 15:
-                    self.emoticons_master_list.append(''.join(temp_list))
-                emoticon_flag = False
-                temp_list = []
-            if emoticon_flag:
-                temp_list.append(c)
+            if c == ')' and emoticon_stack.size():
+                emoticon_stack.pop()
+                self.emoticons_master_list.append(''.join(temp_emoji_list))
+                temp_emoji_list = []
+            if emoticon_stack.size():
+                temp_emoji_list.append(c)
             if c == '(':
-                emoticon_flag = True
+                emoticon_stack.push('(')
              # links
             link_sc = re.match('[/:.\w]', c)
             if position in http_index_list or position in https_index_list:
                 link_flag = True
                 link += c
             if link_flag:
-                temp_list.append(c)
+                temp_link_list.append(c)
             if link_flag and c == ' ' or link_flag and last_position:
-                self.links_master_list.append(''.join(temp_list))
+                self.links_master_list.append(''.join(temp_link_list))
                 link_flag = False
-                temp_list = []
+                temp_link_list = []
             position +=1
 
     def get_link_titles(self):
